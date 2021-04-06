cb05e18c029eee7125eaf8dbbcde1cb6e9714eaa
Ceejayski <sirchijioke1@gmail.com>
2021-04-06 11:36:08 +0100

    Create new functions

--- js/app.js
+++ js/app.js
@@ -43,6 +43,65 @@ class UI {
     list.appendChild(card);
   }
 
+  readBtn = (book) => {
+    const { read } = book;
+    let text;
+    if (read === true) {
+      text = 'Already Read this book';
+    } else {
+      text = 'Book not yet Read';
+    }
+    return text;
+  }
+
+  showAlert = (className, message) => {
+    const form = document.querySelector('.container');
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `notification ${className}`;
+    div.innerHTML = '<button class="delete"></button>';
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.form-container');
+
+    form.insertBefore(div, container);
+
+    setTimeout(() => {
+      document.querySelector('.notification').remove();
+    }, 5000);
+  }
+
+  deleteBook(target) {
+    if (target.classList.contains('delete')) {
+      target.parentElement.remove();
+    } else if (target.classList.contains('remove')) {
+      if (window.confirm('Are You Sure')) {
+        target.parentElement.parentElement.remove();
+        this.showAlert('is-danger', 'Book Removed');
+      }
+    }
+  }
+
+  clearFields = () => {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('genre').value = '';
+    document.getElementById('pages').value = '';
+  }
+
+  readBook(target) {
+    const bid = target.parentElement.id;
+    const book = library[bid];
+    if (target.id === 'read-link') {
+      if (book.read === true) {
+        book.read = false;
+        this.showAlert('is-danger', 'Book not yet Read');
+      } else {
+        book.read = true;
+        this.showAlert('is-success', 'Book Read');
+      }
+      target.innerText = `${this.readBtn(book)}`;
+    }
+  }
 }
 
 document.getElementById('book-form').addEventListener('submit', (e) => {
